de6393a3740c760f67b8e3a767a08635cfe65ad8
aodhon <aodhon@gmail.com>
2012-07-30 01:25:28 +0100

    Added project

---
+++ src/com/miningcoop/ctf/CTF.java
@@ -0,0 +1,14 @@
+package com.miningcoop.ctf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class CTF extends JavaPlugin{
+	public void onEnable(){
+		getLogger().info("CTF plugin enabled");
+	}
+	public void onDisable(){
+		getLogger().info("CTF plugin disabled");
+	}
+}
